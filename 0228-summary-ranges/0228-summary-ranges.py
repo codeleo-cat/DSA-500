c1948bc0605c3dabb9fa7f68881b7ed4b8cff605
codeleo-cat (코드살쾡이) <codeleocat@gmail.com>
2024-11-11 15:56:47 +0900

    Time: 0 ms (100%), Space: 16.5 MB (51.44%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -2,16 +2,19 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums:
             return []
-        
-        ranges = []
-        start = nums[0]
 
-        for i in range(1, len(nums)+1):
-            if i==len(nums) or nums[i] - nums[i-1] !=1:
-                if start == nums[i-1]:
-                    ranges.append(f"{start}")
+        output = []
+        start = nums[0]  # 구간의 시작 값 초기화
+
+        for i in range(1, len(nums) + 1):
+            # 끝에 도달했거나 연속 구간이 끊어질 때
+            if i == len(nums) or nums[i] != nums[i - 1] + 1:
+                # 시작 값과 끝 값이 같으면 단일 값으로 추가
+                if start == nums[i - 1]:
+                    output.append(str(start))
                 else:
-                    ranges.append(f"{start}->{nums[i-1]}")
-                if i<len(nums):
+                    output.append(f"{start}->{nums[i - 1]}")
+                # 새로운 구간의 시작 값을 설정
+                if i < len(nums):
                     start = nums[i]
-        return ranges
+        return output
