8245befeea755f51877c6fb2565a20c0877af9b1
codeleo-cat (코드살쾡이) <codeleocat@gmail.com>
2024-11-15 14:17:05 +0900

    Time: 10 ms (39.94%), Space: 16.8 MB (6.69%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,4 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x_list = [num for num in str(x)]
-        return x_list[:] == x_list[::-1]
+        # x_list = [num for num in str(x)]
+        # return x_list[:] == x_list[::-1]
+        return str(x) == str(x)[::-1]
